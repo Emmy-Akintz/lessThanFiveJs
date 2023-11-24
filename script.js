87efee3a6739786aca9040618008dedd6c55c132
Ayomide Akintan <emzyakints2005@gmail.com>
2023-11-24 20:33:51 +0100

    update

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 const randomNumTemplate = Math.random() * 10
 const randomNum = Math.round(randomNumTemplate)
 
-let output = document.querySelector("output")
+let userInput = document.getElementById("userinput")
+console.log(userInput);
 
 // console.log(randomNumTemplate);
 // console.log(randomNum);
 
 for (i = 0; i < 5; i++) {
-    const userInput = prompt("Input a number that must be less than the random number which is less than or equal to 10 and you have 5 attempts!")
+    document.getElementById("query").innerHTML = "Input a number that must be less than the random number which is less than or equal to 10 and you have 5 attempts!"
+    // const userInput = prompt("Input a number that must be less than the random number which is less than or equal to 10 and you have 5 attempts!")
+
 
     if (userInput < randomNum) {
-        alert("The number is less than the random number")
+        document.getElementById("output").innerHTML = "The number is less than the random number"
     } else if (userInput > randomNum) {
-        alert("The number is greater than the random number")
+        document.getElementById("output").innerHTML = "The number is greater than the random number"
     } else if (userInput = randomNum) {
-        alert(`You have guessed right and the value is ${randomNum}`)
+        document.getElementById("output").innerHTML = `You have guessed right and the value is ${randomNum}`
         break
     } else {
-        alert("You lost!<br>The value is " + randomNum)
+        document.getElementById("output").innerHTML = "You lost!<br>The value is " + randomNum
     }
 }
